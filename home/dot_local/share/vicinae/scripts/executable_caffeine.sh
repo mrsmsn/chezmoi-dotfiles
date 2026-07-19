11bff1660e6089f67c13aefea5f297ac7e3ab6bf
mrsmsn <53550779+mrsmsn@users.noreply.github.com>
2026-07-19 12:40:15 +0900

    Add caffeine-toggle for vicinae

---
+++ home/dot_local/share/vicinae/scripts/executable_caffeine.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title Caffeine (toggle)
+# @vicinae.mode silent
+# @vicinae.packageName System
+# @vicinae.icon ☕
+noctalia msg caffeine-toggle
